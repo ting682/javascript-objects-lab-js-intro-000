b5a35bf7c75df16bc3ba99116b60a72e2157e1f7
Ting Chung <tchung682@gmail.com>
2020-05-12 15:27:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object;
 }
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj;
+}
